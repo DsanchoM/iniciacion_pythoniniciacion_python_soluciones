c775b2f66de3958415dfddba9dccb04e04d7662d
Gabriel Utrilla Peña <95936193+mcgabi@users.noreply.github.com>
2024-10-16 17:40:07 +0200

    Add files via upload

---
+++ Ejercicio_2.py
@@ -0,0 +1,35 @@
+
+print ("--------------------------------------")
+print ("--                                  --")
+print ("--              Hola!               --")
+print ("--                                  --")
+print ("--   Escriba su nombre por favor:   --")
+print ("--                                  --")
+print ("--------------------------------------")
+nombre = input()
+
+print ("Gracias", nombre,".")
+
+print ("--------------------------------------")
+print ("--                                  --")
+print ("--    Escriba el numero de horas    --")
+print ("--                                  --")
+print ("--------------------------------------")
+nhoras = int(input())
+print ("Sus horas son", nhoras,".")
+
+print ("--------------------------------------")
+print ("--                                  --")
+print ("--  Escriba el precio de las horas  --")
+print ("--                                  --")
+print ("--------------------------------------")
+phoras = int(input())
+print ("El precio de las horas es", phoras,"€/h.")
+
+print ("--------------------------------------")
+print ("--                                  --")
+print ("-- El bruto de sus horas es de",(phoras*nhoras),"€""  --")
+print ("--                                  --")
+print ("--------------------------------------")
+
+
