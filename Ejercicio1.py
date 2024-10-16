50f4db349b0752553ff8a5dee7bff10bf48832f5
Gabriel Utrilla Peña <95936193+mcgabi@users.noreply.github.com>
2024-10-16 16:05:12 +0200

    Add files via upload

---
+++ Ejercicio1.py
@@ -0,0 +1,11 @@
+print ("--------------------------------------")
+print ("--                                  --")
+print ("--              Hola!               --")
+print ("--                                  --")
+print ("--   Escriba su nombre por favor:   --")
+print ("--                                  --")
+print ("--------------------------------------")
+
+nombre = input()
+
+print ("GRACIAS!!!", nombre,".")
