f1ecc9dac81a1e100f47f0243ca39cb18dc650cc
lucia-ensenat <gradolucia2022@gmail.com>
2024-10-16 19:41:00 +0200

    Ejercicio3

---
+++ ejercicios/ejercicio_03.py
@@ -0,0 +1,12 @@
+width = 17
+height = 12.0
+
+expression1 = width // 2
+expression2 = width / 2.0
+expression3 = height / 3
+expression4 = 1 + 2 * 5
+
+print(f"EXPRESSION > width//2  = {expression1}, tipo {type(expression1)}")
+print(f"EXPRESSION > width/2.0 = {expression2}, tipo {type(expression2)}")
+print(f"EXPRESSION > height/3  = {expression3}, tipo {type(expression3)}")
+print(f"EXPRESSION > 1+2*5     = {expression4}, tipo {type(expression4)}")
