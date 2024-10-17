b595b16836827188951b80f8c80ada487879d8d1
Manuel Cofrades Vicente <80260484+manuelcofrades@users.noreply.github.com>
2024-10-17 12:41:41 +0200

    Create ejercicio1.py
    Nombre y Bienvenida

---
+++ ejercicio1.py
@@ -0,0 +1,10 @@
+nombre=input('¿como te llamas?\n')
+print('Bienvenido al curso de programación AI, ' + nombre)
+
+
+# Resultado en Terminal
+# >> nombre=input('¿como te llamas?\n')
+# ¿como te llamas?
+# Manuel
+# >>> print ('Bienvenido al curso de programación AI, ' + nombre)
+# Bienvenido al curso de programación AIManuel
