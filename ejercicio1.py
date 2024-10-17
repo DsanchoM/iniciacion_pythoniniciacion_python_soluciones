0736e55d3289a1067195ea48841bffca21db19ea
Manuel Cofrades Vicente <80260484+manuelcofrades@users.noreply.github.com>
2024-10-17 12:47:36 +0200

    Update ejercicio1.py

--- ejercicio1.py
+++ ejercicio1.py
@@ -1,3 +1,5 @@
+# EJERCICIO 1: Escribe un programa que use input para pedirle a un usuario su nombre y luego le de la bienvenida.
+
 nombre=input('¿como te llamas?\n')
 print('Bienvenido al curso de programación AI, ' + nombre)
 
