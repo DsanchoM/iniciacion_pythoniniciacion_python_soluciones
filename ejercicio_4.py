32984db8b369e22a753e097d85778946d98827a5
DiegoGG1986 <dgonzalezg1986@hotmail.com>
2024-10-17 19:31:24 +0200

    ejercicio_4.py

---
+++ ejercicio_4.py
@@ -0,0 +1,6 @@
+tempC=input(Introduzca el valor de grados Celsius que quiera convertir a Fahrenheit, por favor.\n)
+int(tempC)
+tempF=(tempC*9/5)+32 #Aplico la formula de conversión de Celsius a Fahrenheit.
+print("tempF""' grados Fahrenheit'" )
+
+
