73a17d6e78b854a831bfad06726de403a41f227f
Gabriel Utrilla Peña <95936193+mcgabi@users.noreply.github.com>
2024-10-18 17:11:15 +0200

    Add files via upload

---
+++ Ejercicio_3.py
@@ -0,0 +1,15 @@
+width = 17
+height = 12.0
+
+print("width",width)
+print("height",height)
+
+a = width // 2
+b = width / 2.0
+c = height / 3
+d = 1 + 2 * 5
+
+print(f"a = width//2  = {a}, tipo {type(a)}")
+print(f"b = width/2.0 = {b}, tipo {type(b)}")
+print(f"c = height/3  = {c}, tipo {type(c)}")
+print(f"d = 1+2*5     = {d}, tipo {type(d)}")
