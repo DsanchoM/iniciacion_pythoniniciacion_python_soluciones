c3d9a2a8944627548715a7b4c8ec1b5eec89ae18
Jaime Montañés Ascaso <writer.jaime@gmail.com>
2024-10-19 16:53:17 +0200

    Create ejercicio_3.py

---
+++ ejercicio_3.py
@@ -0,0 +1,30 @@
+"""EJERCICIO 3: Supongamos que ejecutamos las siguientes instrucciones de asignación:
+width = 17
+height = 12.0
+Para cada una de las siguientes expresiones, escriba el valor de la expresión y el tipo (del valor de la expresión). Use el intérprete de Python para verificar sus respuestas.
+
+width//2
+width/ 2.0
+height/ 3
+1+2*5"""
+
+
+width = 17
+height = 12.0
+
+print(width)
+print(height)
+type(width)
+type(height)
+
+print(width//2)
+print(type(width))
+
+print(width//2.0)
+
+print(type(width))
+
+print(height/3)
+print(type(height))
+
+print(1+2*5)
