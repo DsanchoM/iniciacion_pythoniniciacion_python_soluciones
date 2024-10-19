b319a9caefedcb25d2bb694bb4acedc66b9c44c2
DiegoGG1986 <dgonzalezg1986@hotmail.com>
2024-10-19 13:04:52 +0200

    Ejercicio_2.py

--- Ejercicio_2.py
+++ Ejercicio_2.py
@@ -1,3 +1,7 @@
+#EJERCICIO 2: Escribe un programa para pedirle al usuario las horas y la tarifa por hora para calcular el pago bruto.
+
+#SOLUCIÓN:
+
 precio=input("indique el coste por hora de su actual contrato, por favor\n")
 precio=int(precio)
 tiempo=input("Muchas gracias. Ahora indique las horas de uso que marca su contador\n")
