c8dead6b3bb7ce369f2b9ecd8cf9e70dcd25756d
DiegoGG1986 <dgonzalezg1986@hotmail.com>
2024-10-19 11:52:50 +0200

    Ejercicio_2.py

---
+++ Ejercicio_2.py
@@ -0,0 +1,6 @@
+precio=input("indique el coste por hora de su actual contrato, por favor\n")
+precio=int(precio)
+tiempo=input("Muchas gracias. Ahora indique las horas de uso que marca su contador\n")
+tiempo=int(tiempo)
+factura=precio*tiempo
+print ("Debido a los datos introducidos, le indicamos que su factura tiene asciende a un coste de",factura,"euros")
