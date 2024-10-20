5ed83fcaba1f57cb8107df7a7720cd0b49ee6c0b
Carmen Po <13294957+Pocarmen@users.noreply.github.com>
2024-10-20 12:18:16 +0200

    Add files via upload

---
+++ ejercicio_1.py
@@ -0,0 +1,5 @@
+#EJERCICIO 1: Escribe un programa que use input para pedirle a un usuario
+# su nombre y luego le de la bienvenida.
+
+nombre = input("Por favor, introduce tu nombre\n")
+print ("Bienvenid@, {}". format(nombre))
