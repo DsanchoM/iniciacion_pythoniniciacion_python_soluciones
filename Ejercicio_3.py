5f0b5607f197ccaf1f1d23075db855084449664f
Fernando Sevil <48855222+lfsevil@users.noreply.github.com>
2024-10-21 12:58:34 +0200

    Add files via upload
    EJERCICIO 3: Supongamos que ejecutamos las siguientes instrucciones de asignación:
    width = 17
    height = 12.0
    
    Para cada una de las siguientes expresiones, escriba el valor de la expresión y el tipo (del valor de la expresión). Use el intérprete de Python para verificar sus respuestas.
    width//2
    width/ 2.0
    height/ 3
    1+2*5

---
+++ Ejercicio_3.py
@@ -0,0 +1,15 @@
+# EJERCICIO 3: Supongamos que ejecutamos las siguientes instrucciones
+# Fernando Sevil
+width = 17
+height = 12.0
+resultado1 = width//2
+print (f"Operación: width//2. Valor de la variable: {resultado1}, tipo de la variable: {type(resultado1)}/n")
+
+resultado2 = width/2.0
+print (f"Operación: width/2.0. Valor de la variable: {resultado2}, tipo de la variable: {type(resultado2)}/n")
+
+resultado3 = height/3
+print (f"Operación: height/3. Valor de la variable: {resultado3}, tipo de la variable: {type(resultado3)}/n")
+
+resultado4 = 1+2*5
+print (f"Operación: resultado = 1+2.5. Valor de la variable: {resultado4}, tipo de la variable: {type(resultado4)}/n")
