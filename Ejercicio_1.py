b0d05643d5def69a807bf2b34bcde7351035094e
Fernando Sevil <48855222+lfsevil@users.noreply.github.com>
2024-10-21 10:11:50 +0200

    Add files via upload

---
+++ Ejercicio_1.py
@@ -0,0 +1,4 @@
+# EJERCICIO 1: Escribe un programa que use input para pedirle a un usuario su nombre y luego le de la bienvenida.
+# Fernando Sevil
+nombre = input ("¿como te llamas? "\n)
+print ("Bienvenido "+ nombre)
