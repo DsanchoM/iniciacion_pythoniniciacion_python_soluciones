dd6999f9005b8240bc2bc2310be93bd671f651b5
Jeremy Velasco <jeremyvelasco135245@gmail.com>
2024-10-22 18:38:32 +0200

    Ejerecicio_1.py

--- Ejerecicio_1.py
+++ Ejerecicio_1.py
@@ -1,4 +1,4 @@
 #"EJERCICIO 1: Escribe un programa que use input para pedirle a un usuario su nombre y luego le de la bienvenida"
 
 nombre= input("¿cual es su nombre? ")
-print("Bienvenido " + nombre "! ")
+print("Bienvenido " +  nombre )
