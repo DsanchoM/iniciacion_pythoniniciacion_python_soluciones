63ec83f5549e8a1bfc1092d86d64911a05edc4d2
Jeremy Velasco <jeremyvelasco135245@gmail.com>
2024-10-22 19:28:01 +0200

    Ejerecicio2.py

---
+++ Ejerecicio2.py
@@ -0,0 +1,7 @@
+#EJERCICIO 2: Escribe un programa para pedirle al usuario las horas y la tarifa por hora para calcular el pago bruto.
+
+print("Calculadora de pago en bruto" )
+Horas = float(input("Introduzca las horas reralizadas: " ))
+Tarifa = float(input("Introduzca la tarifa por hora: " ))
+
+print("El pago bruto final es de: " + str(Horas * Tarifa) + "EUR" ) 
