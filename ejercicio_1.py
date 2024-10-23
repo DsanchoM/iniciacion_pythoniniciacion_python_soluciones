2e18aa6ba146ae15fa3994bab8d9f9732d71f541
Javier <fjlapenatg@fpvirtualaragon.es>
2024-10-23 20:16:56 +0200

    Update ejercicio_1.py

--- ejercicio_1.py
+++ ejercicio_1.py
@@ -1,5 +1,1 @@
-#EJERCICIO 1: Escribe un programa que use input para pedirle a un usuario
-# su nombre y luego le de la bienvenida.
 
-nombre = input("Por favor, introduce tu nombre\n")
-print ("Bienvenid@, {}". format(nombre))
