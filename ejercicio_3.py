5e6323788ac1454f80c1a37eeb3eeddac956d190
María <35327817+mvi2511@users.noreply.github.com>
2024-10-25 13:40:15 +0200

    Add files via upload

---
+++ ejercicio_3.py
@@ -0,0 +1,8 @@
+# declaramos variables de ancho y alto
+width = float(17)
+height = float(12.0)
+# hacemos los distintos cálculos con sus respectivos tipos
+print(f"la división redondeando de width entre 8 es= {int(width//8)}")
+print(f"la división de width entre 2 es= {float(width/2.0)}")
+print(f"la división de height entre 3 es= {float(height/3)}")
+print(f"1+2*5 es={int(1+2*5)}")
