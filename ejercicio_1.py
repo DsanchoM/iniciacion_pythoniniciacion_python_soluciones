2f393c5096697e23e08b69616c89d39be6618147
María <35327817+mvi2511@users.noreply.github.com>
2024-10-25 12:45:22 +0200

    Add files via upload

---
+++ ejercicio_1.py
@@ -0,0 +1,6 @@
+# Pedir al usuario que ingrese su nombre
+nombre = input("¿Cuál es tu nombre? ")
+
+# Bienvenida
+# La f indica f-string o cadena de formato literal en Python
+print(f"Welcome, {nombre}!")
