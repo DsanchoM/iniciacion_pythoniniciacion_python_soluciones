f294348129822ffc99a7bf188da65f0ef33095ff
Fernando Sevil <48855222+lfsevil@users.noreply.github.com>
2024-11-03 17:54:02 +0100

    Update Ejercicio_7.py

--- Ejercicio_7.py
+++ Ejercicio_7.py
@@ -1,13 +1,10 @@
-# EJERCICIO 7: Escribe un programa para solicitar una puntuaciÛn entre 0.0 y 1.0. Si la puntuaciÛn est· fuera de rango, imprime un mensaje de error. 
+# EJERCICIO 7: Escribe un programa para solicitar una puntuaci√≥n entre 0.0 y 1.0. Si la puntuaci√≥n est√° fuera de rango, imprime un mensaje de error. 
 # Fernando Sevil
-
-import string
-
 
 while True:
 
     try:
-        puntuacion = float(input("Dame una puntuaciÛn entre 0 y 1 \n"))
+        puntuacion = float(input("Dame una puntuaci√≥n entre 0 y 1 \n"))
 
         if 0 <= puntuacion <= 1: # evalua si la puntuacion esta dentro del rango
             break # Si es correcto sale del bucle
@@ -15,9 +12,9 @@ while True:
             print ("La puntuacion debe ser superior a 0 y menor que 1. \n")
 
     except ValueError:
-        print("Error: Por favor, introduce valores numÈricos. \n")
+        print("Error: Por favor, introduce valores num√©ricos. \n")
 
-# Asigna el grado seg˙n la puntuaciÛn
+# Asigna el grado seg√∫n la puntuaci√≥n
 if puntuacion >= 0.9:
     Grado = "A"
 elif puntuacion >= 0.8:
