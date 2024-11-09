b129fa2879700ca5c357443c3be8b632df6e32b3
dalray3s <117003016+dalray3s@users.noreply.github.com>
2024-11-09 19:09:17 +0100

    Create ejercicio4.py
    Pasamos valor de entrada en celsius y devolvemos por salida farenheit

---
+++ ejercicio4.py
@@ -0,0 +1,12 @@
+#Solicita la temperatura
+
+#Guardamos el valor en la variable temperatura
+temperatura = (input)
+
+#solicitado el valor
+temperatura=int(input('Cual es la temperatura en Grados Celsius' ))
+ 
+#relizar formula
+resultado = (temperatura * 9 / 5) + 32 
+
+print(f"Me ha dado {temperatura} en grados celsius y en farenheit son {resultado}")
