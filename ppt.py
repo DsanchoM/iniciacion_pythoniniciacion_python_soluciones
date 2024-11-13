a3654e097fb786c2c9f7b190bbfe3c6174dc5e8f
Adrián <sarevok1982@gmail.com>
2024-11-13 20:33:59 +0100

    Piedra papel tijera
    Prueba de subir archivo con github

---
+++ ppt.py
@@ -0,0 +1,32 @@
+import random
+
+def contador (numero:int)->int:
+    numero += 1
+    return numero
+
+var=''
+ganadas_maquina,ganadas_persona = 0,0
+lista = ['piedra', 'papel', 'tijera']
+while var!='salir':
+    var = input('Introduce piedra, papel o tijera: ')
+    if type(var) != type('str') or var not in lista:
+        print('Aprende a escribir')
+    else:
+        maquina=random.choice(lista) #Que elija un elemento de la lista aleatoriamente
+        print(f'La maquina ha sacado: {maquina}' )
+        if maquina == var:
+            print('Empate')
+        elif ((maquina =='piedra' and var=='tijera')
+              or (maquina =='papel' and var=='piedra')
+              or (maquina =='tijera' and var=='papel')):
+            #ganadas_maquina += 1
+            print('La maquina gana')
+            ganadas_maquina = contador(ganadas_maquina)
+        else:
+            #ganadas_persona += 1
+            print('Has ganado')
+            ganadas_persona = contador(ganadas_persona)
+    print(f'Has ganado {ganadas_persona} veces. La maquina ha ganado {ganadas_maquina} veces')
+
+
+
