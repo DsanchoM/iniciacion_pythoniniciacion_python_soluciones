9e8688276b4f91b7d454c0b6d268b943adfeb78b
Adrián <sarevok1982@gmail.com>
2024-11-18 21:58:21 +0100

    Ejercicio 1

---
+++ ejercicio1.py
@@ -0,0 +1,3 @@
+#EJERCICIO 1: Escribe un programa que use input para pedirle a un usuario su nombre y luego le de la bienvenida.
+nombre = input('Hola, ¿como te llamas?: ')
+print(f'¡Bienvenido {nombre}! A estudiar toca')
