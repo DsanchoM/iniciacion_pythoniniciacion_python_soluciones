2aab45d31601174f07e6319d273f835708b39fbe
Adrián <sarevok1982@gmail.com>
2024-11-18 22:12:12 +0100

    Ejercicio2

---
+++ ejercicio2.py
@@ -0,0 +1,4 @@
+#EJERCICIO 2: Escribe un programa para pedirle al usuario las horas y la tarifa por hora para calcular el pago bruto.
+horas = int(input('Introduce las horas que has trabajado: '))
+tarifa = int(input('Introduce la tarifa por hora: '))
+print(f'El pago bruto es {horas*tarifa}')
