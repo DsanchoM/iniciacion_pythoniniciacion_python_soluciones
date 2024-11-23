daa01d0946a9238578b999d816e514f059713472
Adrián <sarevok1982@gmail.com>
2024-11-23 10:32:15 +0100

    He añadido un try except

--- ppt.py
+++ ppt.py
@@ -8,25 +8,25 @@ var=''
 ganadas_maquina,ganadas_persona = 0,0
 lista = ['piedra', 'papel', 'tijera']
 while var!='salir':
-    var = input('Introduce piedra, papel o tijera: ')
-    if type(var) != type('str') or var not in lista:
-        print('Aprende a escribir')
-    else:
-        maquina=random.choice(lista) #Que elija un elemento de la lista aleatoriamente
-        print(f'La maquina ha sacado: {maquina}' )
-        if maquina == var:
-            print('Empate')
-        elif ((maquina =='piedra' and var=='tijera')
-              or (maquina =='papel' and var=='piedra')
-              or (maquina =='tijera' and var=='papel')):
-            #ganadas_maquina += 1
-            print('La maquina gana')
-            ganadas_maquina = contador(ganadas_maquina)
+    try:
+        var = input('Introduce piedra, papel o tijera: ')
+        if type(var) != type('str') or var not in lista:
+            print('Aprende a escribir')
         else:
-            #ganadas_persona += 1
-            print('Has ganado')
-            ganadas_persona = contador(ganadas_persona)
+            maquina=random.choice(lista) #Que elija un elemento de la lista aleatoriamente
+            print(f'La maquina ha sacado: {maquina}' )
+            if maquina == var:
+                print('Empate')
+            elif ((maquina =='piedra' and var=='tijera')
+                  or (maquina =='papel' and var=='piedra')
+                  or (maquina =='tijera' and var=='papel')):
+                #ganadas_maquina += 1
+                print('La maquina gana')
+                ganadas_maquina = contador(ganadas_maquina)
+            else:
+                #ganadas_persona += 1
+                print('Has ganado')
+                ganadas_persona = contador(ganadas_persona)
+    except e:
+        print("Error")
     print(f'Has ganado {ganadas_persona} veces. La maquina ha ganado {ganadas_maquina} veces')
-
-
-
