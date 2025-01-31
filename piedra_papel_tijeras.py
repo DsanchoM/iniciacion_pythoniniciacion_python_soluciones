bf312ab4ba4d10c5c4756a44802b56fbeae95ac8
DiegoGG1986 <dgonzalezg1986@hotmail.com>
2025-01-31 14:09:39 +0100

    piedra_papel_tijeras.py

--- piedra_papel_tijeras.py
+++ piedra_papel_tijeras.py
@@ -12,38 +12,32 @@ contador_jugador=0
 contador_maquina=0
 
 
-def resultado(opcion_jugador,opcion_maquina,contador_jugador,contador_maquina): #Esta función es la que analizará el resultado del juego y llevará el conteo del resultado
-    if opcion_jugador==opcion_maquina:     
+def resultado(opcion_jugador,opcion_maquina): #Esta función es la que analizará el resultado del juego y llevará el conteo del resultado
+    if opcion_jugador==opcion_maquina:
+        ganador=2     
         print("Ambos habeis escogido",opcion_jugador,"y habeis empatado.\n")
     elif opcion_jugador==piedra:
         if opcion_maquina==tijeras:
             print("Enhorabuena. La piedra chafa a las tijeras!!\n")
-            contador_jugador=contador_jugador+1
-            contador_jugador=int(contador_jugador)
+            ganador=0
         else:          
             print("Lástima. El papel envuelve a la piedra.\n")
-            contador_maquina=contador_maquina+1
-            contador_maquina=int(contador_maquina)
+            ganador=1
     elif opcion_jugador==papel:
         if opcion_maquina==piedra:
             print("Yiiijaaaa!!! El papel envolvió a la piedra!!\n")
-            contador_jugador=contador_jugador+1
-            contador_jugador=int(contador_jugador)
+            ganador=0
         else:
             print("Una pena pero tu papel fue cortado por las tijeras...\n")
-            contador_maquina=contador_maquina+1
-            contador_maquina=int(contador_maquina)            
+            ganador=1            
     elif opcion_jugador==tijeras:
         if opcion_maquina==papel:
             print("Toma ya!!Tus tijeras hicieron pedacitos al papel\n")
-            contador_jugador=contador_jugador+1
-            contador_jugador=int(contador_jugador)           
+            ganador=0           
         else:
             print("Ouchh, eso duele... La piedra destrozo tus tijeras\n")
-            contador_maquina=contador_maquina+1
-            contador_maquina=int(contador_maquina)
-    print("El marcador es de:",jugador,contador_jugador,"VS Máquina",contador_maquina,"\n")
-    return contador_jugador,contador_maquina
+            ganador=1
+    return ganador
 
 
 jugador=input("Introduce tu nombre:\n")
@@ -57,7 +51,12 @@ while repetir_partida=="s":
             raise ValueError
         opcion_maquina=random.choice(opciones)
         print("La máquina escogió",opcion_maquina,"\n")
-        resultado(opcion_jugador,opcion_maquina,contador_jugador,contador_maquina)
+        ganador= resultado(opcion_jugador,opcion_maquina)
+        if ganador==0:
+            contador_jugador=contador_jugador+1
+        elif ganador==1:
+            contador_maquina=contador_maquina+1
+        print("El marcador es de:",jugador,contador_jugador,"VS Máquina",contador_maquina,"\n")    
         while True:
             repetir_partida=input("¿Quieres jugar otra partida? (s/n)\n")
             try:
